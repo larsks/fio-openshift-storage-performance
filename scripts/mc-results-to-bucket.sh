204d945738b931971a6b4d3d3fa56cebb020442c
Lars Kellogg-Stedman <lars@redhat.com>
2022-12-15 17:30:21 -0500

    Create volume for mc config
    OpenShift runs pod with random UIDS, so we can't assume we have write
    access to any directories unless we've mounted a volume for that purpose.
    This commit adds a volume for storing the generated minio client
    configuration.

--- scripts/mc-results-to-bucket.sh
+++ scripts/mc-results-to-bucket.sh
@@ -5,13 +5,13 @@ metadata:
 data:
   mc-results-to-bucket.sh: |
     #!/bin/sh
-    set -ue
+    #set -ue
     echo "Setting up mc connection to S3 Bucket:"
-    mc alias set store ${S3Endpoint} ${AWS_ACCESS_KEY_ID} ${AWS_SECRET_ACCESS_KEY} --api S3v4
+    mc -C /config alias set store ${S3Endpoint} ${AWS_ACCESS_KEY_ID} ${AWS_SECRET_ACCESS_KEY} --api S3v4
+
 ##    echo "Syncing contents of ${pwd}/ to Bucket ${BUCKET_NAME}"
 ##    mc mirror ${pwd}/ store/${BUCKET}/${pwd}/
 ##    echo Printing contents of Bucket ${BUCKET}/${pwd}
 ##    echo --------------------------------------------
 ##    mc ls store/${BUCKET}/${pwd}/
 ##    echo --------------------------------------------
----
