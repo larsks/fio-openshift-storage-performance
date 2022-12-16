fa90baa66a2bb6cf8c667c774b0f51b99214d41e
Lars Kellogg-Stedman <lars@redhat.com>
2022-12-15 20:13:18 -0500

    Correct the spelling of some variable names
    In mc-results-to-bucket.sh, correct $pwd to $PWD, and correct $BUCKET
    to $BUCKET_NAME.

--- scripts/mc-results-to-bucket.sh
+++ scripts/mc-results-to-bucket.sh
@@ -9,9 +9,9 @@ data:
     echo "Setting up mc connection to S3 Bucket:"
     mc -C /config alias set store https://${BUCKET_HOST} ${AWS_ACCESS_KEY_ID} ${AWS_SECRET_ACCESS_KEY} --api S3v4
 
-    echo "Syncing contents of ${pwd}/ to Bucket ${BUCKET_NAME}"
-    mc -C /config mirror ${pwd}/ store/${BUCKET}/${pwd}/
-    echo Printing contents of Bucket ${BUCKET}/${pwd}
+    echo "Syncing contents of ${PWD}/ to Bucket ${BUCKET_NAME}"
+    mc mirror ./ store/${BUCKET_NAME}/${BENCHMARK_NAME}/
+    echo Printing contents of Bucket ${BUCKET_NAME}/${BENCHMARK_NAME}
     echo --------------------------------------------
-    mc -C /config ls store/${BUCKET}/${pwd}/
+    mc ls store/${BUCKET_NAME}/${BENCHMARK_NAME}/
     echo --------------------------------------------
