0335873aaa2cb4f64ef8b83ad1e1368455cd48d0
Lars Kellogg-Stedman <lars@redhat.com>
2022-12-15 20:45:38 -0500

    Restore commented code
    Some of the code in mc-results-to-bucket.sh had been commented out.
    In this commit we uncomment the code and make sure it's correct w/r/t
    recent changes.

--- scripts/mc-results-to-bucket.sh
+++ scripts/mc-results-to-bucket.sh
@@ -9,9 +9,9 @@ data:
     echo "Setting up mc connection to S3 Bucket:"
     mc -C /config alias set store https://${BUCKET_HOST} ${AWS_ACCESS_KEY_ID} ${AWS_SECRET_ACCESS_KEY} --api S3v4
 
-##    echo "Syncing contents of ${pwd}/ to Bucket ${BUCKET_NAME}"
-##    mc mirror ${pwd}/ store/${BUCKET}/${pwd}/
-##    echo Printing contents of Bucket ${BUCKET}/${pwd}
-##    echo --------------------------------------------
-##    mc ls store/${BUCKET}/${pwd}/
-##    echo --------------------------------------------
+    echo "Syncing contents of ${pwd}/ to Bucket ${BUCKET_NAME}"
+    mc -C /config mirror ${pwd}/ store/${BUCKET}/${pwd}/
+    echo Printing contents of Bucket ${BUCKET}/${pwd}
+    echo --------------------------------------------
+    mc -C /config ls store/${BUCKET}/${pwd}/
+    echo --------------------------------------------
