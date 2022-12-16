de14266010b8fb90875b19def6c47ca48302e62f
Lars Kellogg-Stedman <lars@redhat.com>
2022-12-15 20:41:22 -0500

    Fix errors reported by shellcheck
    This corrects issues reported by shellcheck [1] (primarily "quote all
    your variables just to avoid problems if they unexpectedly contain
    whitespace or shell metacharacters"). There are a couple of minor
    aesthetic changes as well.
    
    [1]: https://www.shellcheck.net/
    
    x-branch: fix/make-things-work

--- scripts/mc-results-to-bucket.sh
+++ scripts/mc-results-to-bucket.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 set -ue
 echo "Setting up mc connection to S3 Bucket:"
-mc -C /config alias set store https://${BUCKET_HOST} ${AWS_ACCESS_KEY_ID} ${AWS_SECRET_ACCESS_KEY} --api S3v4
+mc -C /config alias set store "https://${BUCKET_HOST}" "${AWS_ACCESS_KEY_ID}" "${AWS_SECRET_ACCESS_KEY}" --api S3v4
 
 while ! [ -f benchmark-complete ]; do
     echo "waiting for benchmark to complete"
     sleep 1
 done
 
-echo "Syncing contents of ${PWD}/ to Bucket ${BUCKET_NAME}"
-mc -C /config mirror . store/${BUCKET_NAME}/${BENCHMARK_NAME}/
-echo Printing contents of Bucket ${BUCKET_NAME}/${BENCHMARK_NAME}
+echo "Syncing contents of ${PWD} to Bucket ${BUCKET_NAME}"
+mc -C /config mirror . "store/${BUCKET_NAME}/${BENCHMARK_NAME}/"
+echo "Contents of Bucket ${BUCKET_NAME}/${BENCHMARK_NAME}"
 echo --------------------------------------------
-mc -C /config ls store/${BUCKET_NAME}/${BENCHMARK_NAME}/
+mc -C /config ls "store/${BUCKET_NAME}/${BENCHMARK_NAME}/"
 echo --------------------------------------------
